2147696bb6ab6016771d65b75b7bb0529bea32bc
Chris Conley <conley.christopher@hotmail.com>
2022-06-23 17:38:25 -0500

    commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,30 +9,30 @@ const data = [
   {
     id: 1, 
     image: IMG4,
-    credit: 'Photo by Markus Spiske on Unsplash',
+    credit: 'Photo by Karl Pawlowicz on Unsplash',
     title: ' Multiple USPS Internal Applications',
-    description: 'The largest applications I work closely with are: Critical Spare Parts - Part management software, Topeka Parts - defect and contact tracking for maintenance, Custodial Workloading - Application for cleaning every postal site nationwide, Dashboard X - Interal dashboard for plant managers and executives to see how each machine/site/district/region is performing, MPETAT - Software used to calculate how many technicians each site needs based on current machines and which of their technicians need training.'
+    description: 'The largest applications I work closely with are: Critical Spare Parts - Part management software, Topeka Parts - defect and contact tracking for maintenance, Custodial Workloading - Application for cleaning every postal site nationwide, Dashboard X - Interal dashboard for people in the field to see how each machine/site/district/region is performing, MPETAT - Software used to calculate how many technicians each site needs based on current machines and which of their technicians need training, and NPA calculations based on the amount of pieces of mail sorted by machines. This gave me greated in depth understanding of the data that we receive from the 10,000s of machines nation-wide and how to calculate at-risk elements, amount of jams and defects, down time, and many other metrics. '
   },
   {
     id: 2, 
     image: IMG1,
-    credit: 'Photo by Daniel Korpai on Unsplash',
+    credit: '<a href="../public/ArcGIS.html">Arc GIS API Testing</a>',
     title: 'ArcGIS API Testing',
-    description: 'Showcasing the ArcGIS API to create a map and perform spatial analysis on the geography of the land. '
+    description: 'Showcasing the ArcGIS API to create a map and perform spatial analysis on the geography of the land. This tests adding a point, line, and polygraph to the ArcGIS basemap layer and utilizes the Buffer, Intersect, and Union geometry functions. Buffer is used to create an extra space around the point to allow it to be easier to see for the user. Intersect then takes the polygon and sees where it will intersect with the buffer and line. Union combines all of the three into one entity allowing the user to create one area. From this point further diving into the API could be done to calculate area, distance, etc. of the selected region of the map. '
   },
   {
     id: 3, 
     image: IMG3,
     credit: 'Photo by Karl Pawlowicz on Unsplash',
     title: 'Internal Employee Resource Center',
-    description: 'Completely overhauled the inplace system with a responsive portal that utilized Bootstrap 4. Also streamlined many php functions and SQL tables on to make them more efficient and allow more pertinent data to be stored and displayed.'
+    description: 'Completely overhauled the inplace system with a responsive portal that utilized Bootstrap 4. The original system was a system of simple HTML pages that displaced help articles, troubleshooting steps, etc. for the support staff. Rewrote the majority of this information in a C.R.U.D. application style so that one front end could populate any article once it was entered into the database. Also streamlined many php functions and SQL tables on to make them more efficient and allow more pertinent data to be stored and displayed.'
   },
   {
     id: 4, 
     image: IMG2,
     credit: 'Photo by Ferenc Almasi on Unsplash',
     title: 'Conference Department Portal',
-    description: 'This was another interal resource project. The Conference Department Portal was a solo project that was a new creation to replace their paper record keeping setup at the time. Setup a LAMP stack from scratch on new hardware, created digital version of all forms and documents required for the department using HTML, CSS, PHP, JS, jQuery, AJAX, JS, and '
+    description: 'This was another interal resource project. The Conference Department Portal was a solo project that was a new creation to replace their paper record keeping setup at the time. Setup a LAMP stack from scratch on new hardware, created digital version of all forms and documents required for the department using HTML, CSS, PHP, JS, jQuery, AJAX, JS, MySQL, and the Bootstrap 3 framework. This application also implemented the MVC framework, CodeIgniter.'
   }
 ]
 
